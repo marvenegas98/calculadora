09c264d97290262d9bd5db13c8935edd336ebdcd
marvenegas98 <m.venegasb98@gmail.com>
2020-08-10 15:17:20 -0600

    Update analizadorSemantico.py

--- analizadorSemantico.py
+++ analizadorSemantico.py
@@ -67,6 +67,6 @@ class AnalizadorSemantico(object):
                 raise Error(
                     'Variable {} no esta definida'.format(item.valor))
             resultado = self.vars[item.valor]
-            return resultado
+            return int(resultado)
     def visitar(self, nodo):
         return getattr(self, nodo.nombre.lower(), self.saltar)(iter(nodo.items))
